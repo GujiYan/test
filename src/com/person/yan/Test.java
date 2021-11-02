b5891c3cc96e3f569300678af5d463d68f4c9ae8
Administrator <vllvii@163.com>
2021-11-02 15:01:21 +0800

    增加功能：判断数组元素是否大于5个

--- src/com/person/yan/Test.java
+++ src/com/person/yan/Test.java
@@ -5,4 +5,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("测试！");
     }
+
+    void testMax(int[] nums){
+        if (nums.length > 5) System.out.println("元素大于5个！");
+    }
 }
